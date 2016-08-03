4d9c3f323f55a3c5d41e3ce7cb56ab651bf2d106
Svarba <svarba@CZPRCOSVARBA.merck.com>
2016-08-03 13:40:23 +0200

    Mac support sed -> gsed, remove packages

--- packages.sh
+++ packages.sh
@@ -2,8 +2,8 @@
 
 ls packages/ > packages.old
 diff packages.old packages.list > packages.diff
-sed -rn 's/^>\ ((\w[-]?)*)$/\1/p' < packages.diff > packages.new
-sed -rn 's/^<\ ((\w[-]?)*)$/\1/p' < packages.diff > packages.remove
+gsed -rn 's/^>\ ((\w[-]?)*)$/\1/p' < packages.diff > packages.new
+gsed -rn 's/^<\ ((\w[-]?)*)$/\1/p' < packages.diff > packages.remove
 cat packages.remove | xargs apm remove
 cat packages.new | xargs apm install
 rm packages.diff packages.new packages.remove packages.old
